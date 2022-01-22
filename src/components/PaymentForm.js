97df74db276536ca92a4a744227417931a27dcee
Daniel <33129456+DanielPelnar@users.noreply.github.com>
2022-01-22 16:57:20 +0000

    Payment page (#4)
    * Changed payment page name
    
    * Created paymentForm component
    
    * Added other payment fields
    
    * Created handleChange and handleSubmit functions
    
    Co-authored-by: Yung Cher Ho <ycho527@hotmail.com>

---
+++ src/components/PaymentForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Form, Button } from "react-bootstrap";
+
+
+export default function PaymentForm() {
+    const [inputs, setInputs] = useState({});
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
+
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formcustomerWalletAddress">
+                <Form.Label>Your Wallet Address</Form.Label>
+                <Form.Control 
+                    type="text"
+                    name="customerWalletAddress"
+                    placeholder="Enter wallet address" 
+                    value={inputs.customerWalletAddress || ""}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formprivateKey">
+                <Form.Label>Your Private Key</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    name="privateKey"
+                    placeholder="Enter private key"
+                    value={inputs.privateKey || ""}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formmerchantWalletAddress">
+                <Form.Label>Merchant's Wallet Address</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    name="merchantWalletAddress"
+                    placeholder="0x329CdCBBD82c934fe32322b423bD8fBd30b4EEB6" 
+                    value="0x329CdCBBD82c934fe32322b423bD8fBd30b4EEB6"
+                    disabled 
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formmercoinType">
+                <Form.Label>Coin</Form.Label>
+                <Form.Control 
+                    type="text"
+                    name="coinType" 
+                    placeholder="Polygon (MATIC)"
+                    value="Polygon (MATIC)"
+                    disabled
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formamount">
+                <Form.Label>Amount</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    name="Amount"
+                    placeholder="19.954382929"
+                    value="19.954382929"
+                    disabled 
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formgasSpend">
+                <Form.Label>Gas Spend</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    name="gasSpend"
+                    placeholder="0.15"
+                    value={inputs.gasSpend || ""}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formtotalAmount">
+                <Form.Label>Total Amount</Form.Label>
+                <Form.Control 
+                    type="number" 
+                    name="totalAmount"
+                    placeholder="0.15" 
+                    value={inputs.totalAmount || 0.0 }
+                    onChange={handleChange}
+                    />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Pay
+            </Button>
+
+        </Form>
+    );
+};
--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,7 +1,14 @@
 import React from "react";
+import PaymentForm from "../components/PaymentForm";
 
 const Payment = () => {
-    return <h1>Payment</h1>;
+    return (
+        <>
+            <h1>Payment page</h1>
+            <PaymentForm />
+        </>
+        
+    );
 };
 
 export default Payment;
