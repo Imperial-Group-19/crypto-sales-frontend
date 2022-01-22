e196165fe00c7c97206909217d3402ea1236d670
Yung Cher Ho <ycho527@hotmail.com>
2022-01-22 11:23:27 +0000

    Created basic routes structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
-function App() {
+import Landing from './pages/Landing';
+import Payment from './pages/Payment';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
