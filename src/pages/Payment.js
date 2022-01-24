a88f1e38d790cd793093caccd15eb5cdd180efff
Yung Cher Ho <ycho527@hotmail.com>
2022-01-24 14:54:23 +0000

    Centred payments form

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,12 +1,17 @@
 import React from "react";
 import PaymentForm from "../components/PaymentForm";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Payment = () => {
     return (
-        <>
-            <h1>Payment page</h1>
-            <PaymentForm />
-        </>
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Payment page</h1>
+                    <PaymentForm />  
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
