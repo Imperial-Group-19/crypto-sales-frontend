c0a550f34aed6a64305ea4d58dd21a5c1cf104a2
Daniel Pelnar <danielpelnar@dyn3218-50.wlan.ic.ac.uk>
2022-01-24 19:23:57 +0000

    styled headline, included ListGroup

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,18 +1,44 @@
 import React, { useState } from "react";
-import { Container, Navbar, Button } from "react-bootstrap";
+import { Container, Navbar, Button, ListGroup, Badge } from "react-bootstrap";
 
 
 export default function Headline() {
     return (
         <Container>
-            <h1>Having problems with your coding interviews?</h1>
-            <h2>Try our new algorithms course. Now available in 3 programming languages</h2>
-            <ul>
-                <li>C++</li>
-                <li>Java</li>
-                <li>Python</li>
-            </ul>
-            <Button variant="outline-success" href="products">See our Courses</Button>
+            <h1 className="display-1">Having problems with your coding interviews?</h1>
+            <p className="lead">Try our new algorithms course. Now available in 3 programming languages</p>
+            <ListGroup as="ol" numbered>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                <div className="fw-bold">C++</div>
+                What a beast this language is. Full of fun and unexpected errors. 
+                But, don't worry! With this course, you will be able to say segfault goodbye! 
+                Learn to implement algorithms in this language in no time.
+                </div>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                <div className="fw-bold">Java</div>
+                Your code will run on every system. Compile once and run anywhere. What more to wish for? Why do blind programmers use Java? Because they can't C. 
+                </div>
+            </ListGroup.Item>
+            <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+            >
+                <div className="ms-2 me-auto">
+                <div className="fw-bold">Python</div>
+                Scared of snakes? Worry no more. Pythons are not venomous, they kill their prey by slowly squeezing it to death. Just remember, as you are coding your algorithms that speed is a factor!
+                </div>
+            </ListGroup.Item>
+            </ListGroup>
+            {/* <Button variant="light" href="products">See our Courses</Button> */}
         </Container>
     )
 }
