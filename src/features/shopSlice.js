034d5dfebe1466f0f0ebbfa79511aa18ab796e47
Yung Cher Ho <ycho527@hotmail.com>
2022-02-22 11:02:26 +0000

    Updated products page to include selection

--- src/features/shopSlice.js
+++ src/features/shopSlice.js
@@ -1,7 +1,51 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  products: [],
+  // products: [],
+  // addedProducts: [],
+  // total: 0,
+  products: [
+    {
+     product_id: "C++ course",
+     title: "C++ course",
+     description: "The original C++ course",
+     features: ["Feature A", "Feature B", "Feature C"],
+     price: 50000,
+     type: "main" 
+    },
+    {
+      product_id: "C++ deluxe course",
+      title: "C++ deluxe course",
+      description: "The upgraded C++ course",
+      features: ["Feature D", "Feature E", "Feature F"],
+      price: 100000,
+      type: "upsell" 
+    },
+    {
+      product_id: "C++ lite course",
+      title: "C++ lite course",
+      description: "The essential C++ course",
+      features: ["Feature G", "Feature H", "Feature I"],
+      price: 25000,
+      type: "downsell" 
+    },
+    {
+      product_id: "Pointers cheatsheet",
+      title: "Pointers cheatsheet",
+      description: "Your quick guide to demystifying pointers",
+      features: ["Feature J", "Feature K", "Feature L"],
+      price: 10000,
+      type: "crosssell" 
+    },
+    {
+      product_id: "STL guide",
+      title: "STL guide",
+      description: "Your guide to the Standard Template Library",
+      features: ["Feature M", "Feature N", "Feature O"],
+      price: 10000,
+      type: "crosssell" 
+    },
+  ],
   addedProducts: [],
   total: 0,
 }
@@ -16,8 +60,8 @@ export const shopSlice = createSlice({
       console.log(state.addedProducts);
       console.log(state.total);
       console.log(state.products);
-      let products = action.payload;
-      state.products = [...products];
+      // let products = action.payload;
+      // state.products = [...products];
 
     },
     addToCart(state, action) {
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,68 +1,119 @@
 import React from "react";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import Header from "../components/Header";
-import { Container, Row, Col, Card, ListGroup, ListGroupItem, Button } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart, removeFromCart } from "../features/shopSlice";
 import ShoppingCart from "../components/ShoppingCart";
 
 export default function Products() {
-    const products = useSelector((state) => state.shop.products);
-    const addedProducts = useSelector((state) => state.shop.addedProducts);
-    const total = useSelector((state) => state.shop.total);
-    const shop = useSelector((state) => state.shop);
-    const dispatch = useDispatch();
+  const allProducts = useSelector((state) => state.shop.products);
+  const addedProducts = useSelector((state) => state.shop.addedProducts);
+  const total = useSelector((state) => state.shop.total);
+  const shop = useSelector((state) => state.shop);
+  const dispatch = useDispatch();
 
-    const productList = (products, addedProducts) => {
-        return(
-            <>
-            <Helmet>
-                <title>Products - detailed information</title>
-                <meta name="description" content="See a detailed information about the products, including their price and buy" />
-            </Helmet>
+  const products = allProducts.filter(
+    (product) => product.type === "crosssell"
+  );
 
-                {products.map(product => (
-                    <Card id = {product.product_id} key={product.product_id} style={{ width: '20rem' }} className="m-2" text="light" bg="secondary" border="light" >
-                        <Card.Body>
-                            <Card.Title>{product.title}</Card.Title>
-                            <Card.Text>
-                            {product.price} MATIC
-                            </Card.Text>
-                            <Card.Text>
-                            {product.description}
-                            </Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            {product.features.map(feature => { return(<ListGroupItem key={feature}>{feature}</ListGroupItem>) })}
-                        </ListGroup>
-                        <Card.Body>
-                            <div className="d-grid gap-2">
-                                {addedProducts.length === 0 || !addedProducts.find(added => added.product_id === product.product_id) ?
-                                    <Button variant="outline-light" size="lg" onClick={() => dispatch(addToCart(product.product_id))}>Add To Cart</Button> :
-                                    <Button variant="outline-dark" size="lg" onClick={() => dispatch(removeFromCart(product.product_id))}>Remove From Cart</Button>
-                                }
-                            </div>
-                        </Card.Body>
-                    </Card>
-                ))}
-            </>
-        )
-    }
-
+  const productList = (products, addedProducts) => {
     return (
-        <>
-            <Header/>   
-            <Container>
-                <Row>
-                    <h1 className="display-1">Our courses</h1>
-                </Row>
-                <Row>
-                    {productList(products, addedProducts)}
-                    {/* <Col xs={3}>
+      <>
+        {products.map((product) => (
+          <Card
+            id={product.product_id}
+            key={product.product_id}
+            style={{ width: "20rem" }}
+            className="m-2"
+            text="light"
+            bg="secondary"
+            border="light"
+          >
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>{product.price} MATIC</Card.Text>
+              <Card.Text>{product.description}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              {product.features.map((feature) => {
+                return <ListGroupItem key={feature}>{feature}</ListGroupItem>;
+              })}
+            </ListGroup>
+            <Card.Body>
+              <div className="d-grid gap-2">
+                {addedProducts.length === 0 ||
+                !addedProducts.find(
+                  (added) => added.product_id === product.product_id
+                ) ? (
+                  <Button
+                    variant="outline-light"
+                    size="lg"
+                    onClick={() => dispatch(addToCart(product.product_id))}
+                  >
+                    Add To Cart
+                  </Button>
+                ) : (
+                  <Button
+                    variant="outline-dark"
+                    size="lg"
+                    onClick={() => dispatch(removeFromCart(product.product_id))}
+                  >
+                    Remove From Cart
+                  </Button>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Products - detailed information</title>
+        <meta
+          name="description"
+          content="See a detailed information about the products, including their price and buy"
+        />
+      </Helmet>
+      <Header />
+      <Container>
+        {/* <Row>
+          <h1 className="display-1">Your current selection</h1>
+        </Row> */}
+        <Row>
+          <Card>
+            <Card.Header as="h3">Your current selection</Card.Header>
+            <Card.Body>
+              <Card.Title>C++ Deluxe</Card.Title>
+              <Card.Text>
+                100000 MATIC
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Row>
+        <Row>
+          <h3 >Our recommended add-ons</h3>
+        </Row>
+        <Row>
+          {productList(products, addedProducts)}
+          {/* <Col xs={3}>
                         <ShoppingCart/>
                     </Col> */}
-                </Row>
-            </Container>
-        </>
-    );
-};
+        </Row>
+      </Container>
+    </>
+  );
+}
