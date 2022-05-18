eddb5874679a6c8c5ac55d4163902b79b346d9e5
vas <wkorobitsyn0@gmail.com>
2022-05-18 11:46:08 +0200

    Fix connection bug when loading merchant products

--- src/merchant/components/Loading.jsx
+++ src/merchant/components/Loading.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useWeb3Context } from "../features/Web3Context";
 
 const Loading = () => {
+  const { connected, handleConnectWallet } = useWeb3Context();
+
+  const checkWeb3Status = async () => {
+    if (!connected) {
+      handleConnectWallet();
+    }
+  };
+
+  useEffect(() => {
+    checkWeb3Status();
+  }, []);
+
   return <div className="centered">Loading...</div>;
 };
 
